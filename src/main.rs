c2afdfce795a6e46da2904d27ffce1d5c059cf52
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-06 21:28:04 -0300

    showing pokemon logo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::fs;
+use owo_colors::{DynColors, OwoColorize};
+
+const POKEMON_LOGO: &str = r#"
+
+"#;
+
+
+
+
+
+fn print_pokemon_logo() {
+
+    let pokemon_ascii = fs::read_to_string("pokemon_ascii.txt")
+        .expect("Something went wrong reading the file");
+
+    let colors: [DynColors; 4] = [
+      "#ffcb05", "#2a75bb", "#c7a008", "#3c5aa6"
+    ].map(|color| color.parse().unwrap());
+
+
+    for line in pokemon_ascii.split_inclusive("\n") {
+        for character in line.chars() {
+              match character {
+                  '#' => print!("{}", character.color(colors[0])),
+                  '.' => print!("{}", character.color(colors[3])),
+                  c if "*%".contains(c) => print!("{}", character.color(colors[2])),
+                  c if "+-:=".contains(c) => print!("{}", character.color(colors[1])),
+                  c => print!("{}", c),
+              }
+      }
+    }
+
+}
+
+fn introduction() {
+    let default_color: [DynColors; 1] = [
+      "#ffcb05"
+    ].map(|color| color.parse().unwrap());
+
+
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    print_pokemon_logo();
+
+    let mut running: bool = true;
+
+    while running {
+    
+    }
+
+    
+
 }
