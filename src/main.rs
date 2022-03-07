990de066ae8af95c49495373b95e6741e3b09c8f
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-06 23:36:10 -0300

    doing -> help command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-use std::fs;
 use owo_colors::{DynColors, OwoColorize};
 
-const POKEMON_LOGO: &str = r#"
 
-"#;
-
-
-
-
-
-fn print_pokemon_logo() {
-
-    let pokemon_ascii = fs::read_to_string("pokemon_ascii.txt")
-        .expect("Something went wrong reading the file");
-
-    let colors: [DynColors; 4] = [
+const colors: [DynColors; 4] = [
       "#ffcb05", "#2a75bb", "#c7a008", "#3c5aa6"
     ].map(|color| color.parse().unwrap());
 
+fn print_pokemon_logo() {
+
+    let pokemon_ascii = include_str!("../pokemon_ascii.txt");
 
     for line in pokemon_ascii.split_inclusive("\n") {
         for character in line.chars() {
@@ -29,6 +19,7 @@ fn print_pokemon_logo() {
                   c => print!("{}", c),
               }
       }
+    
     }
 
 }
@@ -38,22 +29,55 @@ fn introduction() {
       "#ffcb05"
     ].map(|color| color.parse().unwrap());
 
+    println!("\n\n\n\n{}", "This is a quick pokedex in which you can look about pokemons, \
+    evolution levels, moves, types, routes encounters, itens locations and much more. \
+    \nEverything is also acording to pokemon radical red. If you need some help to see the commands type help.".color(default_color[0]));
+
 
 }
 
+fn help() {
+    const HELP_TEXT: &str = r#"
+\n\n\n
+This is a pokedex with Radical red changes integrated.
+USAGE: [OPTIONS] [NAME]
 
+[OPTIONS]
+    POKEMON                     - POKEMON NAME/DEX NUMBER
+    ROUTE                       - POKEMON ENCOUNTER IN EACH ROUTE
+    MOVES                       - POKEMONS MOVES
+    POKEMONLOCATION             - WHERE TO FIND A POKEMON
+    CHANGELOG                   - VERSION 2.3 CHANGELOG
+    EVOLUTIONCHANGE             - COMPILED EVOLUTIONS CHANGES
+    HARDCORE                    - HARDCORE/RESTRICTED MODE CHANGES
+    ITEMSLOCATION               - ITEMS/TM/MOVE TUTOR LOCATIONS
+    BUGS                        - KNOW BUGS LIST
+    HARDCORETRAINERS            - HARDCORE BOSS TRAINERS
+    "#;
+
+    println!("{}", HELP_TEXT.color(colors[0]));
+}
 
 
 fn main() {
 
     print_pokemon_logo();
+    introduction();
 
     let mut running: bool = true;
 
     while running {
-    
-    }
 
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input {
+            "help" => help(),
+        }
     
+        
+
+
+    }
 
 }
